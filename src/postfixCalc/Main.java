53574561c45c6f107cafa3b28cb7b1bc3592db47
Irina Belenki <irina.belenki@gmail.com>
2015-06-02 23:02:58 +0300

    Fixed application logic

--- src/postfixCalc/Main.java
+++ src/postfixCalc/Main.java
@@ -2,67 +2,136 @@ package postfixCalc;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class Main {
+	private static Stack<Integer> operandStack = new Stack<Integer>();
+	private static LinkedList<String> operationQueue = new LinkedList<String>();
+	
 	public static void main(String[] args) {
-
-		Stack<Integer> stack = new Stack<Integer>();
-
 		while (true) {
 			try {
 				
-				System.out.println("Enter " + (stack.size() < 2 ? "operand" : "operation") 
-											+ " or 'exit' to exit");
+				System.out.println("Enter operand or operation\n"  
+										+ " or 'exit'  command to exit\n"
+										+ " or 'clear' command to clear stack and queue\n"
+										+ " or 'print' command to print stack and queue");
 				
 				String line = new BufferedReader(new InputStreamReader(System.in)).readLine();
-				if (line.equals("exit")) {
+				if (line.trim().equals("exit")) {
 					break;
 				}
-				
-				if (stack.size() < 2) {
+				else if (line.trim().equals("clear")) {
+					operandStack.clear();
+					operationQueue.clear();
+					continue;
+				}
+				else if (line.trim().equals("print")) {
+					printStack();
+					printQueue();
+					continue;
+				}
+				else {
+					boolean operandFound = false;
+					boolean operationFound = false;
 					try {
-						int operand = Integer.parseInt(line);
-						stack.push(operand);
+						int operand = Integer.parseInt(line.trim());
+						operandStack.push(operand);
+						operandFound = true;
+						continue;
 					} 
 					catch (NumberFormatException nfe) {						
-						System.err.println("Invalid integer");
+						operandFound = false;
+					}
+					
+					if(containsOperation(line)) {
+						operationFound = true;
+						operationQueue.addLast(line.trim());
+					}
+					else {
+						operationFound = false;
+					}
+					
+					if(!operandFound && !operationFound) {
+						System.out.println("Invalid input");
 						continue;
 					}
-				} 
-				else {
+					
+					if(operandStack.size() < 2) {
+						System.out.println("Number of operands less than two");
+						continue;
+					}
+					
 					int result = 0;
 					int secondOperand = 0;
-					if (line.equals("+")) {
-						result = stack.pop() + stack.pop();
+					String operation = operationQueue.removeFirst();
+					if (operation.equals("+")) {
+						result = operandStack.pop() + operandStack.pop();
 					}
-					else if(line.equals("*")) {
-						result = stack.pop() * stack.pop();
+					else if(operation.equals("*")) {
+						result = operandStack.pop() * operandStack.pop();
 					}
-					else if(line.equals("-")) {
-						secondOperand = stack.pop();
-						result = stack.pop() - secondOperand;
+					else if(operation.equals("-")) {
+						secondOperand = operandStack.pop();
+						result = operandStack.pop() - secondOperand;
 					}
-					else if(line.equals("/")) {
-						secondOperand = stack.pop();
+					else if(operation.equals("/")) {
+						secondOperand = operandStack.pop();
 						if(secondOperand == 0) {
 							System.err.println("Cannot divide by zero");
 							continue;
 						}
-						result = stack.pop() / secondOperand;
+						result = operandStack.pop() / secondOperand;
 					}
-					else {
-						System.err.println("Invalid operation");
-						continue;
-					}
-					stack.push(result);
+					operandStack.push(result);
 					System.out.println("Result: " + result);
+					operationFound = true;
+					
+					
 				}
 			} catch (Exception e) {
 				System.err.println("Exception:" + e.getMessage());
 				continue;
 			}
 		}
+	}
+	
+	public static void printStack() {
+		if(operandStack.size() == 0) {
+			System.out.println("Operand stack is empty");
+			return;
+		}
+		System.out.println("Operands:");
+		Iterator<Integer> it = operandStack.iterator();
+		while(it.hasNext()) {
+			System.out.print(it.next() + " ");
+		}
+		System.out.print("\n");
+	}
+	
+	public static void printQueue() {
+		if(operationQueue.size() == 0) {
+			System.out.println("Operation queue is empty");
+			return;
+		}
+		System.out.println("Operations:");
+		Iterator<String> it = operationQueue.iterator();
+		while(it.hasNext()) {
+			System.out.print(it.next() + " ");
+		}
+		System.out.print("\n");
+	}
+	
+	public static boolean containsOperation(String line) {
+		if (line.trim().equals("+") || 
+			line.trim().equals("-") ||
+			line.trim().equals("*") ||
+			line.trim().equals("/")) {
+			return true;
+		}
+		return false;
 	}
 
 }
